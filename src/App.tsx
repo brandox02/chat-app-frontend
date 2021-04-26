5c68505aa685da514e24961c7545b8777c5dc7dd
brandon <brandonprogamer02@gmail.com>
2021-04-25 23:49:55 -0400

    the backend url have been updated to new name in heroku

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,6 @@ import React from 'react';
 import Background from './components/Background/Background';
 import { Provider } from 'react-redux';
 import store from './redux';
-import { emiteCloseConnection } from './socket/emitters';
-
-
-window.addEventListener('beforeunload', function (e) {
-  setTimeout(() => {
-
-    emiteCloseConnection();
-    e.preventDefault();
-    e.returnValue = 'Enserio?';
-  }, 2000);
-});
 
 console.log('production is running', process.env.REACT_APP_HOST_ONLINE);
 
