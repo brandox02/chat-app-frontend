854485733d895b50abc1d94c9e8cf3d72c7a0336
brandon <brandonprogamer02@gmail.com>
2021-04-25 23:48:39 -0400

    last changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 import Background from './components/Background/Background';
 import { Provider } from 'react-redux';
 import store from './redux';
-import { emiteCloseConnection } from './socket/emitters';
-
-
-window.addEventListener('beforeunload', function (e) {
-  setTimeout(() => {
-
-    emiteCloseConnection();
-    e.preventDefault();
-    e.returnValue = 'Enserio?';
-  }, 2000);
-});
-
-
 
 function App() {
   return (
